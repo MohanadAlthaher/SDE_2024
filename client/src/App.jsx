e6b17f4b5ff6bbfc9a6218889ed2fb41f7c1c5b0
roaa12114 <roaa12114@gmail.com>
2024-10-15 00:03:20 +0200

    Initial project upload

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+import Signup from './Signup';
+
+const App = () => {
+  return (
+    <div>
+     <Signup />
+    </div>
+  );
+};
+
+export default App;
